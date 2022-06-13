687d5c0ad911b6db9c209796b26679a383405a02
Mehmet ali Çakmak <79807829+mehmet5643@users.noreply.github.com>
2022-06-13 19:36:31 +0300

    Lesson 1
    We create basic function for web3

---
+++ py_web3_01.py
@@ -0,0 +1,20 @@
+from web3 import Web3
+
+node_provider="https://mainnet.infura.io/v3/be0cfd2feae44415822699ca2fc1a5ed"
+
+web3_connection=Web3(Web3.HTTPProvider(node_provider))
+#are we connect ?
+def are_connect():
+    print(web3_connection.isConnected())
+
+
+#this function is show us what is last block
+def last_block():
+    print(web3_connection.eth.block_number)
+
+#balance function how much eth in our balance
+def balance(ETH):
+    balance=web3_connection.eth.get_balance(ETH)
+    balance_eth=web3_connection.eth.fromWei(balance,"ether")
+    print(balance_eth)
+
